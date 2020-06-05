9c0397e90e7202a067bf4d30142afb853c2152b4
root <928835514@qq.com>
2020-06-05 14:27:50 +0800

    第一次提交（pojo类）

---
+++ src/main/java/com/offcn/goods/pojo.java
@@ -0,0 +1,23 @@
+package com.offcn.goods;
+
+public class pojo {
+
+    private Integer id;
+    private String name;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
